58a63748ebf2f9d30908bda66140d859e4e78ede
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2024-09-05 23:55:24 +0100

    cusom exception and error response trait

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Traits\ApiResponseTrait;
 
 abstract class Controller
 {
-    //
+    use ApiResponseTrait;
+
+    public function init()
+    {
+        return $this->okResponse("success", ["Handiwork Api Version 1"]);
+    }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,37 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Traits\ApiExceptionTrait;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $exception, $request) {
+            // handle specific exceptions with custom error handler
+            if (
+                $exception instanceof NotFoundHttpException ||
+                $exception instanceof MethodNotAllowedHttpException ||
+                $exception instanceof ModelNotFoundException ||
+                $exception instanceof AuthenticationException ||
+                $exception instanceof ValidationException
+            ) {
+
+                // Handle the exceptions using ApiExceptionTrait
+                return app(ApiExceptionTrait::class)->renderApiException($exception);
+            }
+        });
     })->create();
