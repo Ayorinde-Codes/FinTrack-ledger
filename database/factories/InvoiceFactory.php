8fe99f83a85771e58f6ff666749f4b7cd30d760d
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2024-11-08 23:14:28 +0100

    payment unit test

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Client;
 use App\Models\User;
-use App\Models\Invoice;
 use App\Enums\Status;
 use Illuminate\Support\Str;
 
